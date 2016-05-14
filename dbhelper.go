609314987d370d0352ba0f588640da7dbf82a89f
JabberWocky256 <jabberwocky256@gmail.com>
2016-05-14 20:21:03 +0300

    add new category user

--- dbhelper.go
+++ dbhelper.go
@@ -9,9 +9,33 @@ import (
 var db *sql.DB;
 
 type user_st struct {
-	ID       int
-	Login    string
-	Password string
+	ID               int
+	Login            string
+	Password         string
+	FirstName        string
+	LastName         string
+	Country          string
+	City             string
+	University       string
+	Start_study      int64
+	End_study        int64
+	Age              int
+	Work             string
+	Known_technology string
+	About            string
+}
+
+type company_st struct {
+	Id                  int
+	Login               string
+	Password            string
+	FirstName           string
+	LastName            string
+	Country             string
+	City                string
+	Phone               string
+	Site_domain_address string
+	Description         string
 }
 
 func SetUp() int {
@@ -41,10 +65,51 @@ func GetUser(inputLogin string) (interface{}, int) {
 		var ID int
 		var login string
 		var password string
-		if st, err := checkDbErr(rows.Scan(&ID, &login, &password)); st {
+		var firstName string
+		var lastName string
+		var country string
+		var city string
+		var university string
+		var start_study int64
+		var end_study int64
+		var age int
+		var work string
+		var known_technology string
+		var about string
+		if st, err := checkDbErr(rows.Scan(&ID, &login, &password, &firstName, &lastName, &country, &city,
+			&university, &start_study, &end_study, &age, &work, &known_technology, &about)); st {
 			return nil, err
 		}
-		return user_st{ID, login, password}, 0
+		return user_st{ID, login, password, firstName, lastName, country, city, university, start_study, end_study, age, work, known_technology, about}, 0
+	}
+	return nil, 0
+}
+
+func GetUserByToken(token int) (interface{}, int) {
+	rows, err := db.Query("SELECT * FROM Users WHERE id=?", token)
+	if st, err := checkDbErr(err); st {
+		return nil, err
+	}
+	if rows.Next() {
+		var ID int
+		var login string
+		var password string
+		var firstName string
+		var lastName string
+		var country string
+		var city string
+		var university string
+		var start_study int64
+		var end_study int64
+		var age int
+		var work string
+		var known_technology string
+		var about string
+		if st, err := checkDbErr(rows.Scan(&ID, &login, &password, &firstName, &lastName, &country, &city,
+			&university, &start_study, &end_study, &age, &work, &known_technology, &about)); st {
+			return nil, err
+		}
+		return user_st{ID, login, password, firstName, lastName, country, city, university, start_study, end_study, age, work, known_technology, about}, 0
 	}
 	return nil, 0
 }
@@ -56,6 +121,90 @@ func CreateUser(user user_st) (err int) {
 	}
 
 	if _, err := stmt.Exec(user.Login, user.Password); err != nil {
+		if st, e := checkDbErr(err); st {
+			return e
+		}
+	}
+	return 0
+}
+
+func UpdateUser(user user_st) (err int) {
+	stmt, error := db.Prepare("update Users SET firstName=?, lastname=?, country=?, city=?, university=?, start_study=?, end_study=?," +
+	"age=?, work=?, known_technology=?, about=? where id=?")
+	if st, err := checkDbErr(error); st {
+		return err;
+	}
+
+	if _, err := stmt.Exec(user.FirstName, user.LastName, user.Country, user.City, user.University, user.Start_study, user.End_study,
+		user.Age, user.Work, user.Known_technology, user.About, user.ID); err != nil {
+		if st, e := checkDbErr(err); st {
+			return e
+		}
+	}
+	return 0
+}
+
+func GetCompany(inputLogin string) (interface{}, int) {
+	rows, err := db.Query("SELECT * FROM Companies WHERE login=?", inputLogin)
+	if st, err := checkDbErr(err); st {
+		return nil, err
+	}
+	if rows.Next() {
+		var ID int
+		var login string
+		var password string
+		var firstName string
+		var lastName string
+		var country string
+		var city string
+		var phone string
+		var site_domain_address string
+		var description string
+		var about string
+		if st, err := checkDbErr(rows.Scan(&ID, &login, &password, &firstName, &lastName, &country, &city, &phone,
+			&site_domain_address, &description, &about)); st {
+			return nil, err
+		}
+		return company_st{ID, login, password, firstName, lastName, country, city, phone, site_domain_address, description}, 0
+	}
+	return nil, 0
+}
+
+func GetCompanyByToken(token string) (interface{}, int) {
+	rows, err := db.Query("SELECT * FROM Users WHERE id=?", token)
+	if st, err := checkDbErr(err); st {
+		return nil, err
+	}
+	if rows.Next() {
+		var ID int
+		var login string
+		var password string
+		var firstName string
+		var lastName string
+		var country string
+		var city string
+		var phone string
+		var site_domain_address string
+		var description string
+		var about string
+		if st, err := checkDbErr(rows.Scan(&ID, &login, &password, &firstName, &lastName, &country, &city, &phone,
+			&site_domain_address, &description, &about)); st {
+			return nil, err
+		}
+		return company_st{ID, login, password, firstName, lastName, country, city, phone, site_domain_address, description}, 0
+	}
+	return nil, 0
+}
+
+func CreateCompany(company company_st) (err int) {
+	stmt, error := db.Prepare("insert Companies SET login=?, password=?, firstName=?, lastName=?, country=?, city=?, " +
+	"phone=?, site_domain_address=?, description=?")
+	if st, err := checkDbErr(error); st {
+		return err;
+	}
+
+	if _, err := stmt.Exec(company.Login, company.Password, company.FirstName, company.LastName, company.Country, company.City,
+		company.Phone, company.Site_domain_address, company.Description); err != nil {
 		if st, e := checkDbErr(err); st {
 			return e
 		}
--- serv.go
+++ serv.go
@@ -9,17 +9,49 @@ import (
 	"errors"
 )
 
-type sign_up_struct struct {
+type user_sign_up_struct struct {
 	Login    string
 	Password string
 }
 
-func handlerSignUp(w http.ResponseWriter, r *http.Request) {
+type user_add_info_struct struct {
+	Token            int
+	FirstName        string
+	LastName         string
+	Country          string
+	City             string
+	University       string
+	Start_study      int64
+	End_study        int64
+	Age              int
+	Work             string
+	Known_technology string
+	About            string
+}
+
+type user_get_info_struct struct {
+	Token int
+}
+
+type company_sign_up_struct struct {
+	ID                  int
+	Login               string
+	Password            string
+	FirstName           string
+	LastName            string
+	Country             string
+	City                string
+	Phone               string
+	Site_domain_address string
+	Description         string
+}
+
+func handlerSignUpUser(w http.ResponseWriter, r *http.Request) {
 	if err := SetUp(); err != 0 {
 		printErr(w, errors.New(fmt.Sprintf("cannot setUp db %d", err)))
 	}
 	decoder := json.NewDecoder(r.Body)
-	var t sign_up_struct
+	var t user_sign_up_struct
 	err := decoder.Decode(&t)
 	if err != nil {
 		fmt.Printf("error parse json for sigh up %v", err)
@@ -49,15 +81,16 @@ func handlerSignUp(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func handlerLogIn(w http.ResponseWriter, r *http.Request) {
+func handlerSignInUser(w http.ResponseWriter, r *http.Request) {
 	if err := SetUp(); err != 0 {
 		printErr(w, errors.New(fmt.Sprintf("cannot setUp db %d", err)))
 	}
 	decoder := json.NewDecoder(r.Body)
-	var t sign_up_struct
+	var t user_sign_up_struct
 	err := decoder.Decode(&t)
 	if err != nil {
 		fmt.Printf("error parse json for sigh up %v", err)
+		printErr(w, err)
 	}
 
 	user, e := GetUser(t.Login);
@@ -80,6 +113,107 @@ func handlerLogIn(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func handlerAddUserInfo(w http.ResponseWriter, r *http.Request) {
+	if err := SetUp(); err != 0 {
+		printErr(w, errors.New(fmt.Sprintf("cannot setUp db %d", err)))
+	}
+	decoder := json.NewDecoder(r.Body)
+	var t user_add_info_struct
+	err := decoder.Decode(&t)
+	if err != nil {
+		fmt.Printf("error parse json for sigh up %v", err)
+		printErr(w, err)
+	}
+
+	if (t.Token == 0) {
+		printErr(w, errors.New("token is empty"))
+	}
+
+	user, e := GetUserByToken(t.Token);
+	if e != 0 || user == nil {
+		printErr(w, errors.New(fmt.Sprintf("GetUser error %d", e)))
+		return
+	}
+
+	if err := UpdateUser(user_st{t.Token, "", "", t.FirstName, t.LastName, t.Country, t.City, t.University, t.Start_study, t.End_study,
+		t.Age, t.Work, t.Known_technology, t.About}); err != 0 {
+		printErr(w, errors.New(fmt.Sprintf("create user error %d", err)))
+		return;
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func handlerGetInfoUser(w http.ResponseWriter, r *http.Request) {
+	if err := SetUp(); err != 0 {
+		printErr(w, errors.New(fmt.Sprintf("cannot setUp db %d", err)))
+	}
+	decoder := json.NewDecoder(r.Body)
+	var t user_get_info_struct
+	err := decoder.Decode(&t)
+	if err != nil {
+		fmt.Printf("error parse json for sigh up %v", err)
+		printErr(w, err)
+	}
+
+	if (t.Token == 0) {
+		printErr(w, errors.New("token is empty"))
+	}
+	fmt.Print("token: " + t.Token)
+	user, e := GetUserByToken(t.Token);
+	if e != 0 || user == nil {
+		printErr(w, errors.New(fmt.Sprintf("GetUser error %d", e)))
+		return
+	}
+	if (user != nil) {
+		u, ok := user.(user_st)
+		if !ok {
+			printErr(w, errors.New("can't cast to user_st"))
+			return
+		}
+
+		jsonAnswer := fmt.Sprintf("{\"token\":\"%v\"}", u.ID)
+		w.Write([]byte(jsonAnswer))
+	}
+
+}
+
+func handlerSignUpCompany(w http.ResponseWriter, r *http.Request) {
+	if err := SetUp(); err != 0 {
+		printErr(w, errors.New(fmt.Sprintf("cannot setUp db %d", err)))
+	}
+	decoder := json.NewDecoder(r.Body)
+	var t company_sign_up_struct
+	err := decoder.Decode(&t)
+	if err != nil {
+		fmt.Printf("error parse json for sigh up %v", err)
+	}
+
+	user, e := GetCompany(t.Login);
+	if e != 0 {
+		printErr(w, errors.New(fmt.Sprintf("GetUser error %d", e)))
+		return
+	}
+	if (user != nil) {
+		c, ok := user.(company_st)
+		if !ok {
+			printErr(w, errors.New("can't cast to company_st"))
+		}
+		if ok && c.Login == t.Login {
+			printErr(w, errors.New("dublicate login"))
+			return
+		}
+	}
+
+	if err := CreateCompany(company_st{Login:t.Login, Password:t.Password, FirstName:t.FirstName, LastName:t.LastName,
+		Country:t.Country, City:t.City, Phone:t.Phone, Site_domain_address:t.Site_domain_address, Description:t.Description}); err != 0 {
+		printErr(w, errors.New(fmt.Sprintf("create company error %d", err)))
+		return;
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
 func printErr(w http.ResponseWriter, err error) {
 	if err != nil {
 		fmt.Printf("err with db: %v \n", err)
@@ -88,7 +222,10 @@ func printErr(w http.ResponseWriter, err error) {
 }
 
 func main() {
-	http.HandleFunc("/signup", handlerSignUp)
-	http.HandleFunc("/login", handlerLogIn)
+	http.HandleFunc("/user_signup", handlerSignUpUser)
+	http.HandleFunc("/user_signin", handlerSignInUser)
+	http.HandleFunc("/user_addInfo", handlerAddUserInfo)
+	http.HandleFunc("/user_getInfo", handlerGetInfoUser)
+	http.HandleFunc("/company_signup", handlerSignUpCompany)
 	http.ListenAndServe(":8080", nil)
 }
